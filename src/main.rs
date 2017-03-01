3bf54a27998007856a71b7d466496c71f07fe8b5
Daan Rijks <daanrijks@gmail.com>
2017-03-01 21:14:39 +0100

    Create the example scene in a separate function

--- src/main.rs
+++ src/main.rs
@@ -31,19 +31,8 @@ mod scene;
 mod texture;
 
 fn main() {
-    let mut scene = Scene::new(vec3(0.05, 0.05, 0.10).into());
-
-    scene.add_primitive(Primitive::plane(Material::solid(checkerboard(color!(1.0), color!(0.0), (0.5, 0.5)), 1.0),
-                                         vec3(0.0, -1.0, 0.0),
-                                         Vector::unit_y()));
-    scene.add_primitive(Primitive::sphere(Material::solid(color!(1.0, 0.0, 0.0), 1.0),
-                                          vec3(0.0, 0.0, 2.0),
-                                          1.0));
-
-    scene.add_light(Light::point(vec3(1.5, 1.5, 1.5).into(), vec3(0.0, 1.0, 0.5)));
-
     let raytracer = Raytracer {
-        scene: scene,
+        scene: example_scene(),
         camera: Camera::new(Point::zero(), Vector::unit_z(), 90.0f32.to_radians()),
     };
 
@@ -71,4 +60,33 @@ fn main() {
         .unwrap()
         .write_image_data(&output_data)
         .unwrap();
+}
+
+fn example_scene() -> Scene {
+    let mut scene = Scene::new(vec3(0.05, 0.05, 0.10).into());
+
+    scene.add_primitive(
+        Primitive::plane(
+            Material::solid(
+                checkerboard(color!(1.0), color!(0.0), (0.5, 0.5)),
+                1.0,
+            ),
+            vec3(0.0, -1.0, 0.0),
+            Vector::unit_y(),
+        )
+    );
+
+    scene.add_primitive(
+        Primitive::sphere(
+            Material::solid(color!(1.0, 0.0, 0.0), 1.0),
+            vec3(0.0, 0.0, 2.0),
+            1.0,
+        )
+    );
+
+    scene.add_light(
+        Light::point(vec3(1.5, 1.5, 1.5).into(), vec3(0.0, 1.0, 0.5)),
+    );
+
+    scene
 }
